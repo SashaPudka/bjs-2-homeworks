2af583f3e57dc242ce2b3e9d85d6c11e7f2941f7
Sasha Pudkova <sashapudkova@bk.ru>
2024-05-25 13:33:40 +0300

    the sixth task is verification

--- 6.exception-closure/task.js
+++ 6.exception-closure/task.js
@@ -1,1 +1,53 @@
-﻿
+﻿ //Задача 1//
+ function parseCount (number) {
+
+   if (isNaN(number)) {         //если написать (number === NaN), почему это не одно и тоже//
+    throw new Error('Невалидное значение');
+ }
+ return Number.parseFloat(number);
+}
+
+function validateCount(number){
+    try {
+        return parseCount(number);
+    } catch (error) {
+        return error;
+    }
+}
+
+//Задача 2//
+class Triangle {
+    constructor(a, b, c) {
+        if (a+b<c || a+c<b || b+c<a) {
+            throw new Error("Треугольник с такими сторонами не существует");
+        }
+       this.a = a;
+       this.b = b;
+       this.c = c;
+    }
+    getPerimeter(){
+        let Perimeter = this.a + this.b + this.c;
+      return Perimeter;
+    } 
+    getArea(){
+        let halfPerimeter = (this.a + this.b + this.c) / 2;
+        return Number(Math.sqrt(halfPerimeter*(halfPerimeter-this.a)*
+        (halfPerimeter-this.b)*(halfPerimeter-this.c))).toFixed(3);
+    }
+}
+
+function getTriangle(aLength, bLength, cLength) {
+    try {
+        return new Triangle(aLength, bLength, cLength);
+    } catch (error) {
+        return new Object ({
+        getPerimeter() {
+            return 'Ошибка! Треугольник не существует';
+        },
+        getArea() {
+            return 'Ошибка! Треугольник не существует';
+        }   
+     })
+   }
+}
+    
