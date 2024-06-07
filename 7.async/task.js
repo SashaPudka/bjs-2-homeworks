9c8112ecbd4a737373e0ae3ec07dfd9102379df4
Sasha Pudkova <sashapudkova@bk.ru>
2024-06-07 13:09:08 +0300

    seven task

--- 7.async/task.js
+++ 7.async/task.js
@@ -8,7 +8,7 @@ class AlarmClock {
         if (!time || !callback) {
           throw new Error('Отсутствуют обязательные аргументы');
         }
-        else if (this.intervalId === time) {
+        else if (this.alarmCollection.find(item => item.time === time)) {
           console.warn('Уже присутствует звонок на это же время');
         }
         this.alarmCollection.push({
@@ -18,36 +18,34 @@ class AlarmClock {
     }
 
     removeClock(time){  //удаляет звонки по определённому времени
-      this.alarmCollection.filter(time => this.intervalId !== time);
+      this.alarmCollection = this.alarmCollection.filter(item => item.time !== time);
     }
 
     getCurrentFormattedTime(){ //возвращает текущее время
       return new Date().toTimeString().slice(0, 5);
     }
-    start(){  //запускает будильник
-      if (isNaN(this.intervalId) === true){
+
+    start() {  //запускает будильник
+      if (this.intervalId){
         return;
       }
-      setInterval(()=>{
-        this.intervalId = this.alarmCollection.forEach(function(time, i) {
-          if(this.time[i] === this.getCurrentFormattedTime & 
-          this.canCall[i] === true){
-             this.canCall[i] === false;
-             this.callback();}
-          })
-        },1000)
+      this.intervalId = setInterval(() => {
+        this.alarmCollection.forEach(item => { 
+          if (item.time === this.getCurrentFormattedTime() && item.canCall === true)
+             item.canCall = false;
+             item.callback();
+            }         
+          )}
+        ,1000)
     }
         
-
     stop(){  //останавливает выполнение интервала будильника
       clearInterval(this.intervalId);
-      this.intervalId === null;
+      this.intervalId = null;
     }
 
     resetAllCalls(){  //сбрасывает возможность запуска всех звонков
-      this.alarmCollection.forEach(function(item, i){
-        this.canCall[i] === true;
-      })
+      this.alarmCollection.forEach(item => item.canCall = true)
     }
 
     clearAlarms(){  //удаляет все звонки
